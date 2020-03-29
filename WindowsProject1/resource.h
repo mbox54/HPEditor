3497be57d20e93ea67c25bdfd6067645b1c1bbf5
Korchagin Vyacheslav <mbox54@gmail.com>
2020-03-29 18:19:21 +0700

    define dialog grid actions interface

--- WindowsProject1/resource.h
+++ WindowsProject1/resource.h
@@ -3,6 +3,7 @@
 // Used by WindowsProject1.rc
 //
 #define IDC_MYICON                      2
+#define IDCANCEL2                       3
 #define IDD_WINDOWSPROJECT1_DIALOG      102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
@@ -20,6 +21,19 @@
 #define IDC_EDIT2                       1001
 #define IDC_EDIT_LOG                    1001
 #define IDC_STATIC_CANVAS               1002
+#define IDC_BUTTON_FILE_NEW             1003
+#define IDC_BUTTON_FILE_LOAD            1004
+#define IDC_BUTTON_FILE_SAVE            1005
+#define IDC_STATIC_ROW_COUNT            1006
+#define IDC_STATIC_COL_COUNT            1007
+#define IDC_BUTTON_ROW_INC              1008
+#define IDC_BUTTON_ROW_DEC              1009
+#define IDC_BUTTON_ROW_SET              1010
+#define IDC_BUTTON_COL_INC              1011
+#define IDC_BUTTON_COL_DEC              1012
+#define IDC_BUTTON_COL_SET              1013
+#define IDC_STATIC_ROW_COUNT2           1014
+#define IDC_STATIC_COL_COUNT2           1015
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -29,7 +43,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
